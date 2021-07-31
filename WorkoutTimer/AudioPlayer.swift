96407856ed2216efc1a5b19755a4580ccde83ee4
Yamasaki Kouya <koya2170@gmail.com>
2021-07-31 15:02:11 +0900

    Modify AudioPlayer so that another music app can be played in background.

--- WorkoutTimer/AudioPlayer.swift
+++ WorkoutTimer/AudioPlayer.swift
@@ -10,26 +10,36 @@ import AVFoundation
 
 class AudioPlayer: ObservableObject {
   var player = AVAudioPlayer()
-
+  var session = AVAudioSession.sharedInstance()
+  
   init(name: String, volume: Float = 1) {
+    do {
+      try session.setCategory(.ambient)
+      try session.setActive(true)
+    } catch {
+      print("failed to initialize AVAudioSession")
+    }
+    
     if let url = Bundle.main.url(forResource: name, withExtension:  "m4a") {
       print("success audio file : \(url)")
       do {
-       player = try AVAudioPlayer(contentsOf: url)
+        player = try AVAudioPlayer(contentsOf: url)
         player.prepareToPlay()
         player.setVolume(volume, fadeDuration: 0)
       } catch {
         print("Erro getting audio \(error.localizedDescription)")
       }
+    } else {
+      print("failed audio file : \(name)")
     }
-    print("failed audio file : \(name)")
   }
-
+  
   func play() {
     player.play()
   }
-
+  
   func stop() {
+    print("player stop")
     player.setVolume(0, fadeDuration: 0)
     player.stop()
   }
